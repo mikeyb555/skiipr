a92ac60af79c2365575903957bf62cc22890cc17
Michael Banfield <shrapnelbikes@gmail.com>
2011-11-27 21:23:02 +1000

    added category tests

--- src/test/java/com/skiipr/server/model/DAO/impl/CategoryDaoImplTest.java
+++ src/test/java/com/skiipr/server/model/DAO/impl/CategoryDaoImplTest.java
@@ -7,6 +7,7 @@ package com.skiipr.server.model.DAO.impl;
 import com.skiipr.server.model.Category;
 import com.skiipr.server.model.DAO.CategoryDao;
 import java.util.List;
+import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,22 @@ public class CategoryDaoImplTest {
     @Test
     public void testFindAll(){
         List<Category> categories = categoryDao.findAll();
-        assertEquals(categories.size(), 2);
+        assertEquals(categories.size(), 3);
+    }
+    
+    @Test
+    public void testFindById(){
+        Category category = categoryDao.findByID(1l);
+        Assert.assertEquals(category.getName(), "Basics");
+        
+        
+    }
+    
+    @Test
+    public void testFindByMerchantId(){
+        List<Category> categories = categoryDao.findByMerchantId(1l);
+        Assert.assertEquals(categories.size(), 2);
+        
     }
     
     
